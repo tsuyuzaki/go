a010ceebbeccd25ec7b34a69318fc980df143e59
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-02-19 08:52:59 +0900

    Added 01.04.go

---
+++ GoProgrammingLanguage/chapter01/01.04.go
@@ -0,0 +1,45 @@
+﻿package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "os"
+  "strings"
+)
+
+/**
+ * 入力ファイルが Windowsの改行コード CRLF を利用していると、
+ * "\n"のsplitが動作しないので注意！！
+ * LFの改行の入力ファイルを利用すること。
+ */
+func main() {
+    // https://qiita.com/daigo2010/items/d46975ad6decd8578c45
+    counts := make(map[string]map[string]int)
+    for _, filename := range os.Args[1:] {
+        data, err := ioutil.ReadFile(filename)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+            continue
+        }
+        for _, line := range strings.Split(string(data), "\n") {
+            if line == "" {
+                continue
+            }
+            value, exists := counts[line]
+            if ! exists {
+                value = make(map[string]int)
+                counts[line] = value
+            }
+            value[filename]++
+        }
+    }
+    for line, filenames := range counts {
+        total := 0
+        fmt.Printf("%s:", line)
+        for filename, cnt := range filenames {
+            fmt.Printf("\t%s [%d times]\n", filename, cnt)
+            total += cnt
+        }
+        fmt.Printf("\ttotal [%d times]\n", total)
+    }
+}

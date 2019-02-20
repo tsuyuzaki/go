243ce0bb641b4e99432e22b81379528fb7ddd021
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-02-20 21:23:22 +0900

    Create main.go

---
+++ GoProgrammingLanguage/chapter02/ex01/main.go
@@ -0,0 +1,15 @@
+﻿package main
+
+import (
+    "fmt"
+    "./tmpconv"
+)
+
+func main() {
+    fmt.Println(tempconv.CToF(tempconv.AbsoluteZeroC))
+    fmt.Println(tempconv.CToK(tempconv.AbsoluteZeroC))
+    fmt.Println(tempconv.FToC((tempconv.Fahrenheit)(tempconv.AbsoluteZeroC)))
+    fmt.Println(tempconv.FToK((tempconv.Fahrenheit)(tempconv.AbsoluteZeroC)))
+    fmt.Println(tempconv.KToC((tempconv.Kelvin)(tempconv.AbsoluteZeroC)))
+    fmt.Println(tempconv.KToF((tempconv.Kelvin)(tempconv.AbsoluteZeroC)))
+}

2a13242f905e353ae5a3918037e02258d83a1803
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-02-21 08:56:54 +0900

    Added ex03-05

---
+++ GoProgrammingLanguage/chapter02/ex03-05/popcount.go
@@ -0,0 +1,70 @@
+﻿package main
+
+import (
+    "fmt"
+    "math"
+    "unsafe"
+)
+
+var pc [256]byte
+
+func init() {
+    for i := range pc {
+        pc[i] = pc[i/2] + byte(i & 1)
+    }
+}
+
+func PopCount(x uint64) int {
+    return int(pc[byte(x >> (0 * 8))] +
+        pc[byte(x >> (1 * 8))] +
+        pc[byte(x >> (2 * 8))] +
+        pc[byte(x >> (3 * 8))] +
+        pc[byte(x >> (4 * 8))] +
+        pc[byte(x >> (5 * 8))] +
+        pc[byte(x >> (6 * 8))] +
+        pc[byte(x >> (7 * 8))])
+}
+
+// ex02.03
+func PopCountEx03(x uint64) int {
+    cnt := 0
+    byteSize := int(unsafe.Sizeof(x))
+    for i := 0; i < byteSize; i++ {
+        cnt += int(pc[byte(x >> uint64(i * 8))])
+    }
+    return cnt
+}
+
+// ex02.04
+func PopCountEx04(x uint64) int {
+    cnt := 0
+    bitSize := int(unsafe.Sizeof(x) * 8)
+    for i := 0; i < bitSize; i++ {
+        cnt += int((x >> uint64(i)) & 1)
+    }
+    return cnt
+}
+
+// ex02.05
+func PopCountEx05(x uint64) int {
+    cnt := 0
+    for x > 0 {
+        x &= (x - 1)
+        cnt++
+    }
+    return cnt
+}
+
+func main() {
+    printAll(0)
+    printAll(math.MaxUint64 / 2)
+    printAll(math.MaxUint64)
+}
+
+func printAll(x uint64) {
+    fmt.Println("----")
+    fmt.Printf("%d\n", PopCount(x))
+    fmt.Printf("%d\n", PopCountEx03(x))
+    fmt.Printf("%d\n", PopCountEx04(x))
+    fmt.Printf("%d\n", PopCountEx05(x))
+}
---
+++ GoProgrammingLanguage/chapter02/ex03-05/popcount_test.go
@@ -0,0 +1,38 @@
+﻿package main
+
+import (
+    "math"
+    "testing"
+)
+
+func BenchmarkPopCount(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        PopCount(0)
+        PopCount(math.MaxUint64 / 2)
+        PopCount(math.MaxUint64)
+    }
+}
+
+func BenchmarkPopCountEx03(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        PopCountEx03(0)
+        PopCountEx03(math.MaxUint64 / 2)
+        PopCountEx03(math.MaxUint64)
+    }
+}
+
+func BenchmarkPopCountEx04(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        PopCountEx04(0)
+        PopCountEx04(math.MaxUint64 / 2)
+        PopCountEx04(math.MaxUint64)
+    }
+}
+
+func BenchmarkPopCountEx05(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        PopCountEx05(0)
+        PopCountEx05(math.MaxUint64 / 2)
+        PopCountEx05(math.MaxUint64)
+    }
+}

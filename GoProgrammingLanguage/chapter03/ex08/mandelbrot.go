102f4451fc900a2b353727b8335b4f197d867b1a
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-02-23 13:12:45 +0900

    Modified memo

---
+++ GoProgrammingLanguage/chapter03/ex08/mandelbrot.go
@@ -0,0 +1,46 @@
+﻿package main
+
+import (
+    "image"
+    "image/color"
+    "image/png"
+    "math/cmplx"
+    "os"
+    "math"
+)
+
+const (
+    xmin, ymin, xmax, ymax = -2, -2, +2, +2
+    width, height          = 1024, 1024
+)
+
+func main() {
+    img := image.NewRGBA(image.Rect(0, 0, width, height))
+    for py := 0; py < height; py++ {
+        y := float64(py)/height*(ymax-ymin) + ymin
+        for px := 0; px < width; px++ {
+            x := float64(px)/width*(xmax-xmin) + xmin
+            z := complex(x, y)
+            img.Set(px, py, mandelbrot(z))
+        }
+    }
+    png.Encode(os.Stdout, img)
+}
+
+func mandelbrot(z complex128) color.Color {
+    const iterations = 200
+    const contrast = 15
+
+    var v complex128
+    for n := uint8(0); n < iterations; n++ {
+        v = v*v + z
+        if cmplx.Abs(v) > 2 {
+            px := ((real(z) - xmin) * width / (xmax - xmin))
+            r := uint8(px / 4)
+            g := uint8(math.MaxUint8 * cmplx.Abs(v) / math.Hypot(xmax, ymax))
+            b := uint8(math.MaxUint8 * cmplx.Abs(v) / math.Hypot(xmax, ymax))
+            return color.RGBA{r, g, b, 128}
+        }
+    }
+    return color.Black
+}

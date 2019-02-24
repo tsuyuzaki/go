5968d085674222aad3373bef62a2710c8e2dd82a
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-02-24 10:44:35 +0900

    Create comma.go

---
+++ GoProgrammingLanguage/chapter03/ex12/comma.go
@@ -0,0 +1,62 @@
+﻿package main
+
+import (
+    "bytes"
+    "fmt"
+    "os"
+    "strings"
+    "unicode/utf8"
+)
+
+func main() {
+    for i := 1; i < len(os.Args); i++ {
+        fmt.Printf("  %s\n", comma(os.Args[i]))
+    }
+}
+
+func comma(s string) string {
+    if s == "" {
+        fmt.Fprintf(os.Stderr, "String is empty.\n")
+        return s
+    }
+    
+    splitted := strings.Split(s, ".")
+    len := len(splitted)
+    if len > 2 {
+        fmt.Fprintf(os.Stderr, "String contains multi decimal points.\n")
+        return s
+    }
+    
+    isStartWithSign := (s[0] == '-' || s[0] == '+')
+    
+    var commaInserted string
+    if ! isStartWithSign {
+        commaInserted = commaBeforeDecimalPoint(splitted[0])
+    } else {
+        commaInserted = string(s[0]) + commaBeforeDecimalPoint(splitted[0][1:])
+    }
+    
+    if len == 2 {
+        commaInserted += ("." + splitted[1])
+    }
+    return commaInserted
+}
+
+func commaBeforeDecimalPoint(s string) string {
+    len := len(s)
+    if len != utf8.RuneCountInString(s) {
+        fmt.Fprintf(os.Stderr, "String contains not ASCII character.\n")
+        return s
+    }
+    countForComma := len % 3
+    var buf bytes.Buffer
+    for i := 0; i < len; i++ {
+        buf.WriteByte(s[i])
+        countForComma--
+        if countForComma == 0 && i != (len - 1) {
+            buf.WriteByte(',')
+            countForComma = 3
+        }
+    }
+    return buf.String()
+}

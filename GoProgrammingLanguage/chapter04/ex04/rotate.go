41651e9697b6b3983e8f5ecd58b865478cc32c3f
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-02-24 18:14:42 +0900

    Create rotate.go

---
+++ GoProgrammingLanguage/chapter04/ex04/rotate.go
@@ -0,0 +1,27 @@
+﻿/**
+ * 一回のパスで操作を行うrotateを書きなさい。
+ *
+ * rotateはおそらくp.97の以下の実装。
+ * s := []int{0, 1, 2, 3, 4, 5}
+ * reverse(s[2:])
+ * reverse(s[:2])
+ * reverse(s)  // "[2 3 4 5 0 1]"
+ */
+package main
+
+import (
+    "fmt"
+)
+
+func rotate(s []int, i int) {
+    ret := make([]int, len(s), cap(s))
+    copy(ret, s[i:])
+    copy(ret[i:], s[:i])
+    copy(s, ret)
+}
+
+func main() {
+    s := []int{1, 2, 3, 4, 5}
+    rotate(s, 2)
+    fmt.Println(s)
+}

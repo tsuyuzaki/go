230c76e301d3247ec02febe8b318eae7667add1f
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-11 22:53:16 +0900

    Modified ex11

--- GoProgrammingLanguage/chapter04/ex11/github/common.go
+++ GoProgrammingLanguage/chapter04/ex11/github/common.go
@@ -4,8 +4,68 @@ import (
     "fmt"
     "bufio"
     "os"
+    "strings"
+    "os/exec"
     "encoding/json"
 )
+
+func getFixedCSVInput(csvPath, confirmMsg string) (map[string]string, bool) {
+    openCSV(csvPath)
+    input := readCSV(csvPath)
+    answer := confirm(input, confirmMsg)
+    if answer == "Modify" {
+        input = nil
+        return getFixedCSVInput(csvPath, confirmMsg)
+    }
+    
+    clearCSV(csvPath, "")
+    if answer != "Done" {
+        input = map[string]string{}
+    }
+    return input, (answer == "Done")
+}
+
+func clearCSV(csvPath, formData string) {
+    f, err := os.OpenFile(csvPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "os.OpenFile err[%v]\n", err)
+        os.Exit(1)
+    }
+    defer f.Close()
+    
+    f.Write([]byte(formData))
+}
+
+func openCSV(csvPath string) {
+    cmd := exec.Command(
+        `C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE`,
+        csvPath)
+    err := cmd.Start()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "%v\n", err)
+        os.Exit(1)
+    }
+    cmd.Wait()
+}
+
+func readCSV(csvPath string) map[string]string {
+    f, err := os.Open(csvPath)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "os.Open() error [%v]\n", err)
+        os.Exit(1)
+    }
+    defer f.Close()
+    
+    s := bufio.NewScanner(f)
+    input := make(map[string]string)
+    for s.Scan() {
+        strs := strings.Split(s.Text(), ",")
+        if len(strs) == 2 {
+            input[strs[0]] = strs[1]
+        }
+    }
+    return input
+}
 
 func confirm(input map[string]string, msg string) string {
     jsonstr, _ := json.MarshalIndent(input, "", "    ")
--- GoProgrammingLanguage/chapter04/ex11/github/newissue.go
+++ GoProgrammingLanguage/chapter04/ex11/github/newissue.go
@@ -6,21 +6,23 @@ import (
     "fmt"
     "bytes"
     "encoding/json"
-    "bufio"
-    "os/exec"
-    "strings"
 )
 
 func PostNewIssue() {
-    clearNewIssueCSV()
-    input, ok := getNewIssueInput()
-    clearNewIssueCSV()
+    const (
+        newIssueCSVPath = `NewIssue.csv`
+        formData = `URL,https://api.github.com/repos/test4golang/test/issues
+token,
+title,
+body,`
+        confirmMsg = "Would you like to create new issue?"
+    )
+    clearCSV(newIssueCSVPath, formData)
+    input, ok := getFixedCSVInput(newIssueCSVPath, confirmMsg)
     if ok {
         postNewIssue(input)
     }
 }
-
-const csvPath = `NewIssue.csv`
 
 type newIssue struct {
     url     string
@@ -48,73 +50,10 @@ func createNewIssue(input map[string]string) *newIssue {
         fmt.Fprintf(os.Stderr, "json.Marshal err[%v]\n", err)
         return nil
     }
-
+    
     return &newIssue{url: url, 
         token: fmt.Sprintf("token %s", token), 
         jsonStr: jsonStr}
-}
-
-func getNewIssueInput() (map[string]string, bool) {
-    openNewIssueCSV()
-    input := readNewIssueCSV()
-    answer := confirm(input, "Would you like to create new issue?")
-    if answer == "Done" {
-        return input, true
-    } else if answer == "Cancel" {
-        return map[string]string{}, false
-    } else if answer == "Modify" {
-        input = nil
-        return getNewIssueInput()
-    } else {
-        fmt.Fprintf(os.Stderr, "Invalid user input[%s]\n", answer)
-        return map[string]string{}, false
-    }
-}
-
-func clearNewIssueCSV() {
-    const formData = `URL,https://api.github.com/repos/test4golang/test/issues
-token,
-title,
-body,`
-    file, err := os.OpenFile(csvPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "os.OpenFile err[%v]\n", err)
-        os.Exit(1)
-    }
-    defer file.Close()
-    
-    file.Write([]byte(formData))
-}
-
-func openNewIssueCSV() {
-    cmd := exec.Command(
-        `C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE`,
-        csvPath)
-    err := cmd.Start()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "%v\n", err)
-        os.Exit(1)
-    }
-    cmd.Wait()
-}
-
-func readNewIssueCSV() map[string]string {
-    f, err := os.Open(csvPath)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "os.Open() error [%v]\n", err)
-        os.Exit(1)
-    }
-    defer f.Close()
-    
-    s := bufio.NewScanner(f)
-    input := make(map[string]string)
-    for s.Scan() {
-        strs := strings.Split(s.Text(), ",")
-        if len(strs) == 2 {
-            input[strs[0]] = strs[1]
-        }
-    }
-    return input
 }
 
 func postNewIssue(input map[string]string) {

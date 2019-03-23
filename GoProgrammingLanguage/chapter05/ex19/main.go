efcf9a18e599e2d27b2df14aec6c16fdef5ccb38
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-24 08:58:05 +0900

    Create main.go

---
+++ GoProgrammingLanguage/chapter05/ex19/main.go
@@ -0,0 +1,21 @@
+﻿/**
+ * return 文を含んでいないのに、ゼロ値ではない値を返す関数を panic と recover を使って書きなさい。
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(getValueByPanicAndRecover())
+}
+
+func getValueByPanicAndRecover() (ret int) {
+	defer func() {
+		if p := recover(); p != nil {
+			ret = p.(int)
+		}
+	}()
+	panic(1)
+}

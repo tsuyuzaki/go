ad248e8b76a778af18aa010e8ef8d365f812a9cf
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-24 07:48:42 +0900

    Create fetch.go

---
+++ GoProgrammingLanguage/chapter05/ex18/fetch.go
@@ -0,0 +1,29 @@
+﻿/**
+ * 振る舞いを変えることなく、書き込み可能なファイルを閉じるために defer を使うよう fetch を書き直しなさい。
+ */
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v促n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}

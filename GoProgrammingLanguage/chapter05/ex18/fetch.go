7db7dd189834b4ab351f94e7cd66883d70a8952e
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-24 08:16:55 +0900

    Update fetch.go

--- GoProgrammingLanguage/chapter05/ex18/fetch.go
+++ GoProgrammingLanguage/chapter05/ex18/fetch.go
@@ -5,25 +5,56 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v促n", err)
-			os.Exit(1)
-		}
-		defer resp.Body.Close()
-		
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
+	if len(os.Args) != 2 {
+		fmt.Println(`Please specify 1 url
+  Usage: go run fetch.go https://golang.org/`)
+		return
 	}
+	path, err := fetch(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(path)
+	}
+}
+
+func fetch(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "http.Get(%s) error [%v]\n", url, err)
+		return "", err
+	}
+	defer resp.Body.Close()
+	
+	dirpath := "./result" + resp.Request.URL.Path
+	if err := os.MkdirAll(dirpath, 0777); err != nil {
+		fmt.Fprintf(os.Stderr, "os.MkdirAll(%s) error [%v]\n", dirpath, err)
+		return dirpath, err
+	}
+	path := dirpath + "/index.html"
+	f, err := os.Create(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "os.Create(%s) error [%v]\n", path, err)
+		return path, err
+	}
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "io.Copy() error [%v]\n", err)
+		return path, err
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "Close() error [%s]\n", err)
+		}
+	}()
+	
+	return path, err
 }

462132fdfafa879dace7aa55bbe09cc90d0fcfa1
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-23 19:03:55 +0900

    Create main.go

---
+++ GoProgrammingLanguage/chapter05/ex12/main.go
@@ -0,0 +1,68 @@
+/**
+ * gopl.io/ch5/outline2 (5.5節) の startElement 関数と endElement 関数はグローバル変数 depth を共有しています。
+ * その二つの関数を無名関数にして、outlineに対してローカルな変数を共有するようにしなさい。
+ */
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	pre, post := getFuncs()
+	forEachNode(doc, pre, post)
+
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+func getFuncs() (func(n *html.Node), func(n *html.Node)) {
+	var depth int
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
+	}
+	
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+	return startElement, endElement
+}

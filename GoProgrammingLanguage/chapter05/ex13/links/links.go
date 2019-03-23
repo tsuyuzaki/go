07731cf69f8482f85f016c3716f3106c82935646
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-23 21:16:11 +0900

    Added ex13

---
+++ GoProgrammingLanguage/chapter05/ex13/links/links.go
@@ -0,0 +1,82 @@
+package links
+
+import (
+	"os"
+	"bytes"
+	"io/ioutil"
+	"fmt"
+	"net/http"
+	"net/url"
+	"golang.org/x/net/html"
+)
+
+func writeFileIfOrigHost(b []byte, rawurl string, isOrigHost bool) error {
+	if ! isOrigHost {
+		return nil
+	}
+
+	parsed, err := url.Parse(rawurl)
+	if err != nil {
+		return fmt.Errorf("url.Parse(%s) error [%v]", rawurl, err)
+	}
+	path := "./got" + parsed.Path
+	if err := os.MkdirAll(path, 0777); err != nil {
+		return fmt.Errorf("os.MkdirAll(%s) error %v", path, err)
+	}
+
+	if err := ioutil.WriteFile(path + "/index.html", b, 0755); err != nil {
+		return fmt.Errorf("ioutil.WriteFile error %v", err)
+	}
+	return nil
+}
+
+func Extract(rawurl string, isOrigHost bool) ([]string, error) {
+	resp, err := http.Get(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("getting %s: %s", rawurl, resp.Status)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("ioutil.ReadAll error %v", err)
+	}
+	doc, err := html.Parse(bytes.NewReader(b))
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", rawurl, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+
+	return links, writeFileIfOrigHost(b, rawurl, isOrigHost)
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
---
+++ GoProgrammingLanguage/chapter05/ex13/main.go
@@ -0,0 +1,62 @@
+/**
+ * crawl を修正して、必要に応じてディレクトリを作成しながら見つけたページの複製をローカルに作成するようにしなさい。
+ * 異なるドメインのページの複製はしないようにしなさい。
+ * たとえば、もとのページが golang.org からであれば、そこにあるすべてのファイルは保存しますが、
+ * vimeo.com からのファイルは保存しないということです。
+ */
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"net/url"
+	"./links"
+)
+
+func getOrigHosts(worklist []string) (map[string]bool, error) {
+	hosts := make(map[string]bool)
+	for _, rawurl := range worklist {
+		parsed, err := url.Parse(rawurl)
+		if err != nil {
+			return hosts, fmt.Errorf("url.Parse(%s) error [%v]", rawurl, err)
+		}
+		hosts[parsed.Host] = true
+	}
+	return hosts, nil
+}
+
+func breadthFirst(f func(item string, origHosts map[string]bool) []string, worklist []string) {
+	hosts, err := getOrigHosts(worklist)
+	if err != nil {
+		log.Print(err)
+	}
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item, hosts)...)
+			}
+		}
+	}
+}
+
+func crawl(rawurl string, origHosts map[string]bool) []string {
+	fmt.Println(rawurl)
+	parsed, err := url.Parse(rawurl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	list, err := links.Extract(rawurl, origHosts[parsed.Host])
+	if err != nil {
+		fmt.Println(err)
+	}
+	return list
+}
+
+func main() {
+	breadthFirst(crawl, os.Args[1:])
+}

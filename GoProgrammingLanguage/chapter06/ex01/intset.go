6e2e43c87917a4fc727519b89b7daa9f0878c375
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-03-30 18:42:41 +0900

    Update intset.go

--- GoProgrammingLanguage/chapter06/ex01/intset.go
+++ GoProgrammingLanguage/chapter06/ex01/intset.go
@@ -11,10 +11,8 @@ package intset
 import (
 	"bytes"
 	"fmt"
-	"math"
 	"../popcount"
 )
-
 
 type IntSet struct {
 	words []uint64
@@ -46,7 +44,7 @@ func (s *IntSet) Remove(x int) {
 	if word >= len(s.words) {
 		return // Nothing to do
 	}
-	s.words[word] &= (math.MaxUint64 - (1 << bit))
+	s.words[word] &= ^(1 << bit)
 }
 
 func (s *IntSet) Clear() {

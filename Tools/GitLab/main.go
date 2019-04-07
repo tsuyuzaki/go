dafac69dfd7e093fcd1fd6ec34fa26cbf8687a89
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-04-07 10:28:46 +0900

    Update main.go

--- Tools/GitLab/main.go
+++ Tools/GitLab/main.go
@@ -7,29 +7,29 @@ import (
 	"os"
 )
 
-const kCSVFilePath = "input.csv"
+const csvFilePath = "input.csv"
 
-const kFormData = "#ProjectURL,GroupURL1,GroupURL2,...\n#https://%[1]s/project/path,https://%[1]s/group/path1,https://%[1]s/group/path2,..."
+const formData = "#ProjectURL,GroupAccess(10/20/30/40 or 50),GroupURL1,GroupURL2,...\n#https://%[1]s/project/path,30,https://%[1]s/group/path1,https://%[1]s/group/path2,..."
 
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Please input your token.")
 		return
 	}
-	if !csv.CreateFileIfNotExists(kCSVFilePath, fmt.Sprintf(kFormData, "gitlab.com")) ||
-		!csv.ShowCSV(kCSVFilePath) {
+	if !csv.CreateFileIfNotExists(csvFilePath, fmt.Sprintf(formData, "gitlab.com")) ||
+		!csv.ShowCSV(csvFilePath) {
 		return
 	}
 
-	rows, ok := csv.ReadCSV(kCSVFilePath)
+	rows, ok := csv.ReadCSV(csvFilePath)
 	if !ok {
 		return
 	}
 	for _, splittedRow := range rows {
-		if len(splittedRow) <= 1 {
+		if len(splittedRow) <= 2 {
 			fmt.Fprintf(os.Stderr, "Invalid row[%v].\n", splittedRow)
 			continue
 		}
-		gitlab.AddGroups(os.Args[1], splittedRow[0], splittedRow[1:])
+		gitlab.AddGroups(os.Args[1], splittedRow[0], splittedRow[1], splittedRow[2:])
 	}
 }

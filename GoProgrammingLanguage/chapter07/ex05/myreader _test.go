490907aac8ceb8b7e0ad78a7f0bcc4b98c2fd197
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-05-03 10:40:48 +0900

    Added ex05

---
+++ GoProgrammingLanguage/chapter07/ex05/myreader _test.go
@@ -0,0 +1,58 @@
+package myreader
+
+import (
+	"io"
+	"testing"
+)
+
+type testReader struct {
+	s string
+	cur int
+}
+
+func (r *testReader) Read(p []byte) (int, error) {
+	n := copy(p, r.s[r.cur:])
+	r.cur += n
+	if r.cur < len(r.s) {
+		return n, nil
+	}
+	return n, io.EOF
+}
+
+func NewTestReader(n int64) io.Reader {
+	s := ""
+	for i := int64(0); i < n; i++ {
+		s += "a"
+	}
+	return &testReader{s: s, cur: 0}
+}
+
+func TestReader(t *testing.T) {
+	const maxSize = 10
+
+	tr := NewTestReader(maxSize)
+	r := LimitReader(tr, maxSize)
+	
+	p := make([]byte, maxSize-1)
+	n, err := r.Read(p)
+	if err != nil || n != len(p) {
+		t.Errorf("maxSize=%d, len(p)=%d, error=%v, n=%d\n", maxSize, len(p), err, n)
+	}
+
+	tr = NewTestReader(maxSize)
+	r = LimitReader(tr, maxSize)
+
+	p = make([]byte, maxSize)
+	n, err = r.Read(p)
+	if err != io.EOF || n != maxSize {
+		t.Errorf("maxSize=%d, len(p)=%d, error=%v, n=%d\n", maxSize, len(p), err, n)
+	}
+
+	tr = NewTestReader(maxSize)
+	r = LimitReader(tr, maxSize)
+
+	p = make([]byte, maxSize+1)
+	if err != io.EOF || n != maxSize {
+		t.Errorf("maxSize=%d, len(p)=%d, error=%v, n=%d\n", maxSize, len(p), err, n)
+	}
+}
---
+++ GoProgrammingLanguage/chapter07/ex05/myreader.go
@@ -0,0 +1,34 @@
+/**
+ * io パッケージの LimitReader 関数は io.Reader である r とバイト数 n を受け取り、
+ * r から読み出す別の Reader を返しますが、n バイトを読みだした後にファイルの終わりの状態を報告します。
+ * その関数を実装しなさい。
+ *
+ * func LimitReader(r io.Reader, n int64) io.Reader
+ */
+package myreader
+
+import "io"
+
+type myReader struct {
+	r io.Reader // underlying reader
+	n int64     // max bytes remaining
+}
+
+func (r *myReader) Read(p []byte) (n int, err error) {
+	if int64(len(p)) > r.n {
+		bs := make([]byte, r.n)
+		n, err = r.r.Read(bs)
+		copy(p, bs)
+	} else {
+		n, err = r.r.Read(p)
+	}
+	r.n -= int64(n)
+	if err == nil && r.n <= 0{
+		err = io.EOF
+	}
+	return n, err
+}
+
+func LimitReader(r io.Reader, n int64) io.Reader {
+	return &myReader{r: r, n: n}
+}

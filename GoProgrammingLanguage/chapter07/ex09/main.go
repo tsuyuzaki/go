26febddb96e55f68be49b631528124ceff259be3
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-05-05 10:50:05 +0900

    Added ex09 (リソース共有している点TBD)

---
+++ GoProgrammingLanguage/chapter07/ex09/main.go
@@ -0,0 +1,63 @@
+/**
+ * html/template パッケージ (4.6節) を使って、printTracks を HTML の表として曲を表示する関数で置き換えなさい。
+ * 列の見出しをクリックしたら表をソートするために HTTP リクエストを行うように前の練習問題の解答を使いなさい*2。
+ * *2訳注: この練習問題は、ウェブサーバを作成することを意図しています。
+ */
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"sort"
+	"io/ioutil"
+	"./mytrack"
+	"html/template"
+	"net/http"
+)
+
+var tracks = []*mytrack.Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, mytrack.Length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, mytrack.Length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, mytrack.Length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, mytrack.Length("4m24s")},
+}
+
+func getSortKey(r *http.Request) (string, bool) {
+	query := r.URL.Query()
+	if query == nil {
+		return "", false
+	}
+	value, ok := query["sortkey"]
+	if !ok {
+		return "", false
+	}
+	if len(value) == 0 {
+		return "", false
+	}
+	return value[0], true
+}
+
+func main() {
+	page, err := ioutil.ReadFile("./html/index.html")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ioutil.ReadFile error [%v]\n", err)
+		return
+	}
+	table := template.Must(template.New("table").Parse(string(page)))
+	tracks := mytrack.NewTracks(tracks)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		key, ok := getSortKey(r)
+		if ok {
+			tracks.SetSortKey(key)
+		}
+		
+		sort.Sort(tracks)
+		
+		if err := table.Execute(w, tracks); err != nil {
+			fmt.Fprintf(os.Stderr, "table.Execute error [%v]\n", err)
+		}
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}

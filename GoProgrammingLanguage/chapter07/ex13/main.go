d60a26a2078e30ad1a98f1b99cc84bfb3162244d
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-05-19 15:23:10 +0900

    Added ex13

---
+++ GoProgrammingLanguage/chapter07/ex13/main.go
@@ -0,0 +1,31 @@
+/**
+ * 構文ツリーを綺麗に表示する String メソッドを Expr に追加しなさい。
+ * その結果を再び解析したら同等のツリーになることを検査しなさい。
+ */
+package main
+
+import (
+	"os"
+	"fmt"
+	"math"
+	"./eval"
+)
+
+func main() {
+	expr, err := eval.Parse("sqrt(A / pi)")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "eval.Parse() error. [%v]\n", err)
+		return
+	}
+	s := expr.String()
+	fmt.Println(s)
+
+	reExpr, err := eval.Parse(s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "eval.Parse() error. [%v]\n", err)
+		return
+	}
+	
+	env := eval.Env{"A": 87616, "pi": math.Pi}
+	fmt.Printf("%f, %f", expr.Eval(env), reExpr.Eval(env))
+}

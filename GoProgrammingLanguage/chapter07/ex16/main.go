2f6226f06e8bac6445a52e841f26b828e7308833
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-05-21 20:38:10 +0900

    Update main.go

--- GoProgrammingLanguage/chapter07/ex16/main.go
+++ GoProgrammingLanguage/chapter07/ex16/main.go
@@ -36,41 +36,41 @@ func getExprAndEnv(r *http.Request) (string, string, bool) {
 	return expr, value[0], true
 }
 
-const msg = "Please enter the formula in the URL\nEx: http://localhost:8000/?expr=a*b&env={\"a\":2,\"b\":4} (Ex means 2 * 4.)\n"
+func calcHandler(w http.ResponseWriter, r *http.Request) {
+	strexpr, strenv, ok := getExprAndEnv(r)
+	if !ok {
+		fmt.Fprintf(w, "Please enter the formula in the URL\nEx: http://localhost:8000/?expr=a*b&env={\"a\":2,\"b\":4} (Ex means 2 * 4.)")
+		return
+	}
+	expr, err := eval.Parse(strexpr)
+	if err != nil {
+		fmt.Fprintf(w, "Invalid expr(%s). [%v]\n", strexpr, err)
+		return
+	}
+	env := make(eval.Env)
+	if strenv != "" {
+		err = json.Unmarshal([]byte(strenv), &env)
+		if err != nil {
+			fmt.Fprintf(w, "Invalid env expression. err[%v]\n", err)
+			return
+		}
+	}
+
+	vars := make(map[eval.Var]bool)
+	err = expr.Check(vars)
+	if err != nil {
+		fmt.Fprintf(w, "expr check() error. [%v]\n", err)
+		return
+	}
+	
+	result := expr.Eval(env)
+	tpl := template.Must(template.ParseFiles("./template/index.html.tpl"))
+	if err := tpl.Execute(w, result); err != nil {
+		fmt.Fprintf(os.Stderr, "tpl.Execute error [%v]\n", err)
+	}
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		strexpr, strenv, ok := getExprAndEnv(r)
-		if !ok {
-			fmt.Fprintf(w, msg)
-			return
-		}
-		expr, err := eval.Parse(strexpr)
-		if err != nil {
-			fmt.Fprintf(w, "Invalid expr(%s). [%v]\n", strexpr, err)
-			return
-		}
-		env := make(eval.Env)
-		if strenv != "" {
-			err = json.Unmarshal([]byte(strenv), &env)
-			if err != nil {
-				fmt.Fprintf(w, "Invalid env expression. err[%v]\n", err)
-				return
-			}
-		}
-
-		vars := make(map[eval.Var]bool)
-		err = expr.Check(vars)
-		if err != nil {
-			fmt.Fprintf(w, "expr check() error. [%v]\n", err)
-			return
-		}
-		
-		result := expr.Eval(env)
-		tpl := template.Must(template.ParseFiles("./template/index.html.tpl"))
-		if err := tpl.Execute(w, result); err != nil {
-			fmt.Fprintf(os.Stderr, "tpl.Execute error [%v]\n", err)
-		}
-	})
+	http.HandleFunc("/", calcHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }

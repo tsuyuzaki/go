e264f50807cc76cf8b20d4e5d72dca89e948ccd1
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-05-24 08:57:27 +0900

    Added ex17

---
+++ GoProgrammingLanguage/chapter07/ex17/main.go
@@ -0,0 +1,63 @@
+/**
+ * CSS のように、名前だけではなくその属性でも要素が選択されるように xmlselect を拡張しなさい。
+ * たとえば、<div id="page" class="wide"> などの要素は、その名前だけではなく一致する id や class によって選択できるようにします。
+ */
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	dec := xml.NewDecoder(os.Stdin)
+	var elems []*xml.StartElement
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "xmlselect: %v\n", err)
+			os.Exit(1)
+		}
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			elems = append(elems, &tok)
+		case xml.EndElement:
+			elems = elems[:len(elems)-1]
+		case xml.CharData:
+			names := toNames(elems)
+			//fmt.Println(names)
+			if containsAll(names, os.Args[1:]) {
+				fmt.Printf("%s: %s\n", strings.Join(names, " "), tok)
+			}
+		}
+	}
+}
+
+func toNames(elems []*xml.StartElement) []string {
+	names := make([]string, 0)
+	for _, elem := range elems {
+		names = append(names, elem.Name.Local)
+		for _, attr := range elem.Attr {
+			names = append(names, attr.Name.Local)
+		}
+	}
+	return names
+}
+
+func containsAll(x, y []string) bool {
+	for len(y) <= len(x) {
+		if len(y) == 0 {
+			return true
+		}
+		if x[0] == y[0] {
+			y = y[1:]
+		}
+		x = x[1:]
+	}
+	return false
+}

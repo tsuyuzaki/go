470f441ea0e1c7dfda7217919560f9a950684578
tsuyuzaki <this.is.tsuyu@gmail.com>
2019-05-25 07:18:05 +0900

    Update main.go

--- GoProgrammingLanguage/chapter07/ex09/main.go
+++ GoProgrammingLanguage/chapter07/ex09/main.go
@@ -16,14 +16,14 @@ import (
 )
 
 const sortKeyName = "sortKey"
+var table = template.Must(template.ParseFiles("./html/index.html"))
 
 func getSortKeys(r *http.Request) []string {
 	key := r.URL.Query().Get(sortKeyName)
 	if key == "" {
 		return []string{} // For default order
 	}
-	var keys []string
-	keys = append(keys, key)
+    keys := []string{key}
 	cookie, err := r.Cookie(sortKeyName)
 	if err == nil && cookie.Value != "" {
 		keys = append(keys, cookie.Value)
@@ -31,32 +31,32 @@ func getSortKeys(r *http.Request) []string {
 	return keys
 }
 
-func setSortKeyToCookie(w http.ResponseWriter, key string) {
-	cookie := &http.Cookie{Name: sortKeyName, Value: key}
+func setCookie(w http.ResponseWriter, name, value string) {
+	cookie := &http.Cookie{Name: name, Value: value}
 	http.SetCookie(w, cookie)
 }
 
-func main() {
-	table := template.Must(template.ParseFiles("./html/index.html"))
+func reqHandler(w http.ResponseWriter, r *http.Request) {
+	keys := getSortKeys(r)
+	if len(keys) != 0 {
+		setCookie(w, sortKeyName, keys[0])
+	} else {
+		setCookie(w, sortKeyName, "") // Clear cookie
+	}
+	tracks := []*mytrack.Track{
+		{"Go", "Delilah", "From the Roots Up", 2012, mytrack.Length("3m38s")},
+		{"Go", "Moby", "Moby", 1992, mytrack.Length("3m37s")},
+		{"Go Ahead", "Alicia Keys", "As I Am", 2007, mytrack.Length("4m36s")},
+		{"Ready 2 Go", "Martin Solveig", "Smash", 2011, mytrack.Length("4m24s")},
+	}
+	ts := mytrack.NewTracks(tracks, keys)
+	sort.Sort(ts)
+	if err := table.Execute(w, ts.Tracks); err != nil {
+		fmt.Fprintf(os.Stderr, "table.Execute error [%v]\n", err)
+	}
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		keys := getSortKeys(r)
-		if len(keys) != 0 {
-			setSortKeyToCookie(w, keys[0])
-		} else {
-			setSortKeyToCookie(w, "") // Clear cookie
-		}
-		tracks := []*mytrack.Track{
-			{"Go", "Delilah", "From the Roots Up", 2012, mytrack.Length("3m38s")},
-			{"Go", "Moby", "Moby", 1992, mytrack.Length("3m37s")},
-			{"Go Ahead", "Alicia Keys", "As I Am", 2007, mytrack.Length("4m36s")},
-			{"Ready 2 Go", "Martin Solveig", "Smash", 2011, mytrack.Length("4m24s")},
-		}
-		ts := mytrack.NewTracks(tracks, keys)
-		sort.Sort(ts)
-		if err := table.Execute(w, ts.Tracks); err != nil {
-			fmt.Fprintf(os.Stderr, "table.Execute error [%v]\n", err)
-		}
-	})
+func main() {
+	http.HandleFunc("/", reqHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
